8ae32bcf39d1ce887e591e172965b5e366985ce6
alvaro0106 <alvarollvv@gmail.com>
2021-10-09 13:16:37 -0500

    El primer commit

---
+++ opc_play_book.sh
@@ -0,0 +1,13 @@
+#Opciones de ejecucion
+
+1. -i inventario -> Especifica un fichero, un script o un directorio
+2. --syntax-check playboo.yaml -> Revisa sintaxis de playbook
+3. --list-task -> Lista las tareas del playbook
+4. --step -> Pregunta paso a paso si quieres continuar
+5. --start-at-tas ="tarea" -> Inicia en una tarea Especifica
+6. --force / -f -> Limita las tareas en paralelo (por defecto 5)
+7. ---vvv (vetvoce) -> Muestra mas informacion 
+
+
+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
+
