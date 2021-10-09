38c4d556d71acdea0ab524e6a85a906384aa71d5
alvaro0106 <alvarollvv@gmail.com>
2021-10-09 16:32:17 -0500

    slvando cambios

--- opc_play_book.sh
+++ opc_play_book.sh
@@ -7,8 +7,3 @@
 5. --start-at-tas ="tarea" -> Inicia en una tarea Especifica
 6. --force / -f -> Limita las tareas en paralelo (por defecto 5)
 7. ---vvv (vetvoce) -> Muestra mas informacion 
-
-
-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
-
-uno mas 
